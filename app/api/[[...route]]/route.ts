f5039bdd7970e60200f9faa2f83fbe5a3c58af13
LimeIvy <raimu10311221@yahoo.co.jp>
2025-08-25 23:22:19 +0900

    ✨ Epson APIの追加

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { logger } from "hono/logger";
+import epson from "./epson";
 
 const app = new Hono().basePath("/api");
 
 app.use("*", logger());
+app.route("/epson", epson);
 
 export const GET = handle(app);
 export const POST = handle(app);
