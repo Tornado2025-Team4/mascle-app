924044d539b7ebcb575f0b22312144af4c5047eb
ast <code@ast24.dev>
2025-08-31 08:50:52 +0900

    🐛 APIがFEに奪われる問題修正(/middleware.ts編集)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,18 @@
 import { Hono } from 'hono';
 import { createSupabaseClientMW } from './_cmn/create_supaclient';
 import app_tags from './tags';
-import { ApiError, ApiErrorFatal } from './_cmn/error';
+import { ApiError, ApiErrorFatal, ApiErrorNotFound } from './_cmn/error';
+import { getEnvMW } from './_cmn/get_env';
 
 const app = new Hono();
 
-app.use('*', createSupabaseClientMW);
+app.use('*', getEnvMW, createSupabaseClientMW);
 
 app.route('/tags', app_tags);
+
+app.notFound((c) => {
+    throw new ApiErrorNotFound('API Endpoint');
+});
 
 app.onError(async (err, c) => {
     const castedErr = err instanceof ApiError ? err : new ApiErrorFatal(`unknown error`);
