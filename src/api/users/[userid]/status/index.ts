77ace15c0401dd9cf5a7baa2a566ef8fa4e951be
ast <code@ast24.dev>
2025-09-11 02:19:26 +0900

    コンフリクト修正

--- src/api/users/[userid]/status/index.ts
+++ src/api/users/[userid]/status/index.ts
@@ -5,7 +5,6 @@ import app_users_userid_status_stateid from './[stateid]';
 import { createSupabaseSessMW } from '@/src/api/_cmn/create_supasess';
 import { verifyJwtMW } from '@/src/api/_cmn/verify_jwt';
 import { rejectSpecByAnonMW } from '../_cmn/reject_spec_by_anon';
-import get from './get';
 
 const app_users_userid_status = new Hono();
 
