6d4b7974fee038788f7d32388488c4c2c5222f1d
ast <code@ast24.dev>
2025-09-12 01:28:11 +0900

    無駄に作られたファイル削除

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -3,14 +3,10 @@ import app_users_userid from './[userid]';
 import { createSupabaseSessMWOptional } from '../_cmn/create_supasess';
 import { verifyJwtMWOptional } from '../_cmn/verify_jwt';
 import get from './get';
-// import bootstrap, { bootstrapMW } from './bootstrap';
 
 const app_users = new Hono();
 
 app_users.use(verifyJwtMWOptional, createSupabaseSessMWOptional);
-
-// /api/users/bootstrap: 認証ユーザーの users_master を作成（なければ）
-// app_users.post('/bootstrap', bootstrapMW, bootstrap); // *! DB側のトリガーで作成されるはずなのでコメントアウトして様子見
 
 app_users.get('/', get);
 
